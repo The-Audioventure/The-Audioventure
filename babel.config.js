26d5c8c03f9d9a338c222a31a66b1e5a1ff0b8e8
YahtzeeRage <Yahiya.Hussain001@umb.edu>
2021-02-20 14:16:37 -0500

    fixed jest

--- babel.config.js
+++ babel.config.js
@@ -7,7 +7,9 @@ module.exports = function(api) {
       '@babel/plugin-transform-runtime',
       '@babel/plugin-transform-async-to-generator',
       '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-class-properties',
       '@babel/plugin-proposal-export-namespace-from'
+
   ]
   };
 };
--- src/screens/HomeScreen.test.js
+++ src/screens/HomeScreen.test.js
@@ -1,13 +1,5 @@
 import {describe, expect, test} from '@jest/globals'
 
-describe('test tests', () => {
-    test('test that tests work 1', () => {
-        expect(3,).toBe(3);
-      });
-
-    test('test that tests work 2', () => {
-    expect(4,).toBe(4);
-    });
-})
-
-
+test('test that tests work', () => {
+  expect(3,).toBe(3);
+});
